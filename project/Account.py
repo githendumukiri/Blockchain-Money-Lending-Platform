43d799aee1070724922feb3737d66c78831a5c5e
Githendu <githendumukiri@gmail.com>
2019-12-01 00:29:57 -0500

    getting account class situated

---
+++ project/Account.py
@@ -0,0 +1,10 @@
+class Account:
+    def __init__(self):
+        self._account_number = 0
+        self._balance = 0
+
+    def account_number(self):
+        return self._account_number
+
+    def get_account_number(self, account_number):
+        pass
